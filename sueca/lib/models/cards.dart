1ad9bb60efb6b459b8721d97224da9e51710564a
Rodrigo Dias <rodrigofldias@gmail.com>
2022-09-20 00:51:17 +0100

    .

--- sueca/lib/models/cards.dart
+++ sueca/lib/models/cards.dart
@@ -20,10 +20,10 @@ import 'package:material_design_icons_flutter/material_design_icons_flutter.dart
 
 /// Suit
 ///
-/// 0 - Spades
-/// 1 - Hearts
-/// 2 - Diamonds
-/// 3 - Clubs
+/// 0 - Spades ♠
+/// 1 - Hearts ♥
+/// 2 - Diamonds ♦
+/// 3 - Clubs ♣
 
 class PlayingCard {
   final int _rank;
